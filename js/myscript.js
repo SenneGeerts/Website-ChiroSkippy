1948fed2deb2d3b5f8a10d076eb02e0a548ea0ed
SenneGeerts <72514319+SenneGeerts@users.noreply.github.com>
2023-01-25 15:18:37 +0100

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,26 @@
+/*window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.backgroundColor = "#ffff";
+    document.getElementById("navbar-logo").style.color = "#000000";
+    document.getElementById("nav-item1").style.color = "#000000";
+    document.getElementById("nav-item2").style.color = "#000000";
+    document.getElementById("nav-item3").style.color = "#000000";
+    document.getElementById("nav-item4").style.color = "#000000";
+
+
+    elements = document.getElementsByClassName("collapse navbar-collapse");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor="black";
+    }
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+    document.getElementById("navbar-logo").style.color = "#ffff";
+    document.getElementById("nav-item1").style.color = "#ffff";
+    document.getElementById("nav-item2").style.color = "#ffff";
+    document.getElementById("nav-item3").style.color = "#ffff";
+    document.getElementById("nav-item4").style.color = "#ffff";
+    
+    } 
+} */
